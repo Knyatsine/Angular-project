b9ff08389f776b996ea67acbfa324e646ff3ead7
Kudzai Nyatsine <kudzai.nyatsine@outlook.com>
2017-05-07 15:28:59 +0100

    added more codes

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ app.controller('StoreController',function(){
 	this.product = gem;
 });
 
-var gem = {
+var gem = [
+{
 	name:'Perfume',
 	price:2.95,
 	description:'Smells very beautful, really good for ladies ',
-	canPurchase:false,
-	soldOut:false,
-}
+	canPurchase:true,
+	
+},
 
+{
+	name:'Deodorant',
+	price:5.5,
+	description:'Get rid of those bad smells by using this refreshing product',
+	canPurchase:false,
+
+},
+
+
+];
 })();
